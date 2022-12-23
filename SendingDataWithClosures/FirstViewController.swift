88e2e9ead195871ea0f3c59e4aaa258bf6a95ef3
serkanboyraci <serkanboyraci@gmail.com>
2022-12-23 15:19:12 +0300

    SecondVC added

---
+++ SendingDataWithClosures/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendingDataWithClosures
+//
+//  Created by Ali serkan Boyracı  on 23.12.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
